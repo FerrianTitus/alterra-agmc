10a61028abf499025d68fbd411e008c28c71ea51
Ferrian <ferrian00@gmail.com>
2022-09-15 13:42:03 +0700

    Fix Init + add log middleware

--- day2/dynamic-api/main.go
+++ day2/dynamic-api/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"github.com/ferriantitus/alterra-agmc/day2/dynamic-api/config"
 	"github.com/ferriantitus/alterra-agmc/day2/dynamic-api/routes"
+	m "github.com/ferriantitus/alterra-agmc/day2/dynamic-api/middleware"
 )
 
 func main() {
+	config.InitDB()
 	e := routes.New()
+
+	m.LogMiddleware(e)
 
 	e.Start(":8080")
 

fe8f40c02a39ffc3edde504d6f9923be88727bc7
Ferrian <ferrian00@gmail.com>
2022-09-16 10:20:28 +0700

    Add feature for Day3

--- day2/static-api/main.go
+++ day2/static-api/main.go
@@ -1,9 +1,15 @@
 package main
 
-import "github.com/ferriantitus/alterra-agmc/day2/static-api/routes"
+import (
+	l "github.com/ferriantitus/alterra-agmc/day2/static-api/middleware"
+	"github.com/ferriantitus/alterra-agmc/day2/static-api/routes"
+)
 
 func main() {
 	e := routes.New()
+
+	l.LogsMiddleware(e)
+
 	e.Start(":8080")
 
 	e.Logger.Fatal(e.Start("8000"))
--- day2/static-api/routes/routes.go
+++ day2/static-api/routes/routes.go
@@ -3,12 +3,26 @@ package routes
 import (
 	"github.com/ferriantitus/alterra-agmc/day2/static-api/controllers"
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"os"
 )
 
 func New() *echo.Echo {
 	e := echo.New()
 
 	v1Books := e.Group("/v1")
+
+	v1Books.Use(middleware.JWTWithConfig(middleware.JWTConfig{
+		SigningKey: []byte(os.Getenv("JWT_SECRET")),
+		AuthScheme: "Bearer",
+		Skipper: func(c echo.Context) bool {
+			// Skip middleware if method is equal 'POST'
+			if c.Request().Method == "GET" {
+				return true
+			}
+			return false
+		},
+	}))
 
 	v1Books.GET("/books", controllers.GetAllBooks)
 	v1Books.GET("/books/:id", controllers.GetBook)
